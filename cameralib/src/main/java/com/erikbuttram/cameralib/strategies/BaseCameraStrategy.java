2127e211e550fa7dcd9cff54515bd36e2c33ea10
Erik Buttram <erikb@discreteit.com>
2015-06-24 18:57:55 -0700

    still workin' on it

---
+++ cameralib/src/main/java/com/erikbuttram/cameralib/strategies/BaseCameraStrategy.java
@@ -0,0 +1,11 @@
+package com.erikbuttram.cameralib.strategies;
+
+/**
+ * TODO:
+ * The base class provides all the abstract methods that the
+ * consumers of the api needed to toggle between cameras, instantiate the HAL stuff,
+ * taking pictures, recording video, and clean up.
+ */
+public abstract class BaseCameraStrategy {
+
+}
---
+++ cameralib/src/main/java/com/erikbuttram/cameralib/strategies/CameraStrategy.java
@@ -0,0 +1,7 @@
+package com.erikbuttram.cameralib.strategies;
+
+/**
+ * Created by erikb on 6/24/15.
+ */
+public class CameraStrategy {
+}
